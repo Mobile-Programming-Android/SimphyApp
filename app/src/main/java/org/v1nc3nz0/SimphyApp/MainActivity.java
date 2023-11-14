70f3959512393164cff6e31b09633497743431bd
LittleShyStar02 <65179413+LittleShyStar02@users.noreply.github.com>
2023-11-14 23:19:08 +0100

    Initial commit

---
+++ app/src/main/java/org/v1nc3nz0/SimphyApp/MainActivity.java
@@ -0,0 +1,16 @@
+package org.v1nc3nz0.SimphyApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
---
+++ app/src/main/java/org/v1nc3nz0/SimphyApp/activity/HomeActivity.java
@@ -0,0 +1,5 @@
+package org.v1nc3nz0.SimphyApp.activity;
+
+public class HomeActivity
+{
+}
